73a6e31508c83ff23733bc13b5474bc74c65352e
Rick Wilson <rick@teamwilson.org>
2015-04-13 14:41:00 -0600

    test3

--- Signup-Tutorial-Parse-Swift/AppDelegate.swift
+++ Signup-Tutorial-Parse-Swift/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    // test2
+    // test3
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
